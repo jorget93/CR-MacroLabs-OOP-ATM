6c5e506be939a75da2558b582f7729545161619b
jorge <jorgetapia0293@gmail.com>
2021-03-12 18:25:41 -0500

    Played with CLI

---
+++ src/main/java/Console.java
@@ -0,0 +1,32 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Console {
+
+    public void greetingMessage(){
+        System.out.println("Welcome!");
+    }
+
+    public void chooseAccount(){
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Which account would you like to access?");
+        System.out.println("1 - Checking, 2 - Savings, 3 - Investing");
+
+        int numIn = userInput.nextInt();
+
+        switch (numIn){
+            case 1:  {
+                System.out.println("You have chosen Checking");
+                break;
+            }
+            case 2:  {
+                System.out.println("You have chosen Savings");
+                break;
+            }
+            case 3:  {
+                System.out.println("You have chosen Investing");
+                break;
+            }
+        }
+    }
+}
