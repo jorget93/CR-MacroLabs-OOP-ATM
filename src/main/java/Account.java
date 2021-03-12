80e9801f5ebb5aaca3397dad5c4d33fcce9177e7
Abiel Figueroa <Abe@zipcodes-MacBook-Pro-2.local>
2021-03-12 17:40:46 -0500

    Account class complete

---
+++ src/main/java/Account.java
@@ -0,0 +1,57 @@
+public class Account {
+    Double balance;
+    String accountType= "Checking";
+
+
+    //A new instance of account would need starting funds and account type
+    public Account(Double startingBalance, String typeOfAccount){
+        balance=startingBalance;
+        accountType=typeOfAccount;
+    }
+
+    //the version of the constructor the tests use.
+    //The console should probably use the other one.
+    //If this constructor IS used, account defaults to a checking account.
+    public Account(Double startingBalance){
+        balance=startingBalance;
+    }
+
+    public Double getBalance(){
+        return balance;
+    }
+
+    public String getAccountType(){
+        return accountType;
+    }
+
+
+
+    //Possible area to expand on: perhaps withdrawing works differently pending account type.
+    //prevents overdrawing
+    public Double withdraw(Double amount){
+        if(amount>balance){
+            System.out.println("Insufficient funds for withdrawal.");
+            return 0.0;
+        }else{balance-=amount;
+            return amount;}
+    }
+
+    public void deposit(Double amount){
+        balance+=amount;
+    }
+
+
+    public void transfer(Account gettingMoney, Double amount){
+        gettingMoney.deposit(this.withdraw(amount));
+    }
+
+
+    //Uncertain if this is needed, but it was in the tests.
+    //Returns true if the account can be closed. False if not.
+    public Boolean closeAccount(){
+        if (balance==0.0){return true;}
+        else return false;
+    }
+
+
+}
