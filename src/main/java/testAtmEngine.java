ddff2a750c9e74610033270277247cd4d2274bf3
Abiel Figueroa <Abe@zipcodes-MacBook-Pro-2.local>
2021-03-13 18:32:36 -0500

    account tests begun

---
+++ src/main/java/testAtmEngine.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+/*
+Test checklist:
+Test both constructors
+Check balance
+Test withdrawal (Both with enough and not enough money in account, withdrawing negative amount...)
+Test deposit (make sure positive)
+
+*/
+
+public class testAtmEngine {
+    @Test
+    public void engineConstructor(){}
+}
