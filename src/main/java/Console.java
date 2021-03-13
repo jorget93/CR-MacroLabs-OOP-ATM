3514df437c7632e0a96f0cba3d827c2333c3454c
jorge <jorgetapia0293@gmail.com>
2021-03-12 20:58:24 -0500

    ryans methods

--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -39,4 +39,35 @@ public class Console {
             }
         }
     }
+
+    public void depositOrWithdraw() {
+        System.out.println("Would you like to deposit of withdraw funds?");
+        System.out.println("1 - Deposit, 2 - Withdraw");
+        int numIn = userInput.nextInt();
+        int numIn = "";
+        switch (numIn) {
+            case 1: {
+                this.deposit();
+                break;
+            }
+            case 2: {
+                this.withdraw();
+                break;
+            }
+            default: {
+                System.out.println("Please choose from the menu!");
+            }
+        }
+    }
+    public void deposit() {
+        System.out.println("Amount to be deposited? ");
+        //receive input
+        //parse to int
+        //if not an int return message to try again
+    }
+    public void withdraw() {
+        System.out.println("Amount to be withdrawn? ");
+        if (// input greater than account balance ask for new amount)
+    }
+}
 }
