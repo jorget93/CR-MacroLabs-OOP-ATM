6331cf51e41e096f84fe53fa2a33ae8783156491
jorge <jorgetapia0293@gmail.com>
2021-03-14 18:18:55 -0400

    tests

--- src/main/test/BreakCheckTest.java
+++ src/main/test/BreakCheckTest.java
@@ -3,21 +3,55 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.io.StringWriter;
+
 //@RunWith(Parameterized.class)
 public class BreakCheckTest {
 
     @Test
-    public void pinGeneratorTest(){
+    public void pinGeneratorTest1(){
         //Given
         BreakCheck newCheck = new BreakCheck();
         //Action
         String newPin = newCheck.pinGenerator();
         //Expected
         int expected = 8;
-
+        //Actual
         int actual = newPin.length();
         System.out.println(newPin);
         Assert.assertEquals(expected,actual);
     }
+    @Test
+    public void pinGeneratorTest2(){
+        //Given
+        BreakCheck newCheck = new BreakCheck();
+        //Action
+        String newPin = newCheck.pinGenerator();
+        //Expected
+        int expected = 8;
+        //Actual
+        int actual = newPin.length();
+        System.out.println(newPin);
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void pinGeneratorTest3(){
+        //Given
+        BreakCheck newCheck = new BreakCheck();
+        //Action
+        String newPin = newCheck.pinGenerator();
+        //Expected
+        int expected = 8;
+        //Actual
+        int actual = newPin.length();
+        System.out.println(newPin);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void isDouble(){
+        StringWriter output = new StringWriter();
+    }
+
 
 }
