3038c70ba5491a1e243f32158fc3b20f3e5f167d
jorge <jorgetapia0293@gmail.com>
2021-03-14 20:24:05 -0400

    Added consoleTest

--- src/main/test/ConsoleTest.java
+++ src/main/test/ConsoleTest.java
@@ -1,12 +1,28 @@
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class ConsoleTest{
 
     @Test
     public void getStringInputTest(){
-        Scanner scanner = new Scanner((Readable) System.out);
-
+        // Given, simulated user input
+        Console console = new Console();
+        InputStream sysInBackup = System.in; // backup System.in to restore it later
+        ByteArrayInputStream in = new ByteArrayInputStream("Hello!".getBytes());
+        System.setIn(in);
+        // Expected
+        String expected = "Hello!";
+        // Actual
+        String actual = console.getStringInput(expected);
+        // Assert
+        System.out.println(actual);
+        System.setIn(sysInBackup);
+        Assert.assertEquals(expected,actual);
     }
+
+
 }
